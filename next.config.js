f782fcf79487c4d0fbfe2766c35eca718b315643
Shiv-Sawant <shivsawant@1958@gmail.com>
2024-01-21 21:06:14 +0530

    update next config file

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.freepik.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  distDir:"build"
 }
 
 module.exports = nextConfig
