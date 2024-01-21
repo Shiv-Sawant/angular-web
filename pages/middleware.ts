2efde09b5a3a19804e1e752ad8183c45b9db5623
Shiv-Sawant <shivsawant@1958@gmail.com>
2024-01-21 19:37:23 +0530

    remove middleware.tsx.

--- pages/middleware.ts
+++
@@ -1,9 +0,0 @@
-import dbConnect from './mongoose/db';
-
-export const withMongoose = (fn: any): any => {
-  return async function (...args) {
-    console.log('into with mongoose...');
-    await dbConnect();
-    return await fn(...args);
-  };
-};
